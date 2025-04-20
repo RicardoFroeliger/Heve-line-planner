80f9f00c9dcd0add07d68e49e7d11b6b5579f804
Ricardo <ricardo@connected.nl>
2025-04-20 02:45:38 +0200

    Initialized tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="text-center h-full">
+            <header className="bg-[#282c34] min-h-[100vh] flex flex-col items-center justify-center text-white">
+                <img
+                    src={logo}
+                    className="h-[20vmin] App-logo"
+                    alt="logo"
+                />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
