e6572407b6c0d34052507f7ba136d6198e9fd9bf
Ricardo <ricardo@connected.nl>
2025-04-20 02:50:58 +0200

    Added Lucide icons; Made first UI for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './styles/App.css';
+import { PlusCircleIcon } from 'lucide-react';
+import logo from './logo.png';
+import React, { useState } from 'react';
+
+type Story = {
+    name: string;
+    stories: Story[];
+}
 
 function App() {
+    const [stories, setStories] = useState<Story[]>([]);
+    function createStory() {
+        setStories([
+            ...stories,
+            {
+                name: 'New Story',
+                stories: [],
+            }
+        ]);
+    }
+
     return (
         <div className="text-center h-full">
-            <header className="bg-[#282c34] min-h-[100vh] flex flex-col items-center justify-center text-white">
+            <header className="bg-[#282c34] min-h-[100vh] flex flex-col items-center text-white">
                 <img
                     src={logo}
-                    className="h-[20vmin] App-logo"
+                    className="h-20 absolute top-20 rounded-full border-gray-500 border-2"
                     alt="logo"
                 />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <div className="flex flex-col items-center justify-center flex-grow w-full">
+                    {
+                        !stories.length && (
+                            <h1 className="mb-12 text-2xl">
+                                Create your first story by clicking below
+                            </h1>
+                        )
+                    }
+                    <div className='flex flex-row gap-20 relative'>
+                        <div className="absolute w-[90%] h-1 bg-gray-700 rounded-full z-10 top-1/2 left-1/2 -translate-x-[50%]"></div>
+
+                        <div className="flex flex-row gap-20 relative z-20">
+                            {
+                                stories.map(story => (
+                                    <button
+                                        className="h-10 w-10 rounded-full bg-blue-600 flex justify-center items-center cursor-pointer text-2xl"
+                                        onClick={createStory}
+                                    >
+                                        0
+                                    </button>
+                                ))
+                            }
+                            <button
+                                className="h-10 w-10 rounded-full bg-blue-600 flex justify-center items-center cursor-pointer"
+                                onClick={createStory}
+                            >
+                                <PlusCircleIcon className="h-7 w-7" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </header>
         </div>
     );
